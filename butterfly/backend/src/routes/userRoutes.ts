126290c561002ddff9f714bd4225dcc27ccf4376
abdulkhaliqdev <abdulkhaliqdev@gmail.com>
2024-11-20 04:22:58 +0500

    add the .gitignore file along the other stuff setup

--- butterfly/backend/src/routes/userRoutes.ts
+++ butterfly/backend/src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import bcrypt from 'bcrypt';
+// import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/user';
 import authenticateToken from '../middleware/auth';
@@ -13,11 +13,11 @@ router.post('/register', async (req: Request, res: Response, next: NextFunction)
     const userExists = await User.findOne({ where: { email } });
     if (userExists) { res.status(400).send('User already exists'); }
     else {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const newUser = await User.create({ username, email, password: hashedPassword });
+      // const hashedPassword = await bcrypt.hash(password, 10);
+      // const newUser = await User.create({ username, email, password: hashedPassword });
   
-      const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
-      res.json({ token });
+      // const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
+      // res.json({ token });
     }
   } catch (error) {
     next(error);
@@ -31,13 +31,13 @@ router.post('/login', async (req: Request, res: Response, next: NextFunction): P
     if (!user) {
       res.status(400).send('User not found');
     }else {
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      if (!isPasswordValid) {
-        res.status(400).send('Invalid password');
-      }else {
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
-        res.json({ token });
-      }
+      // const isPasswordValid = await bcrypt.compare(password, user.password);
+      // if (!isPasswordValid) {
+        // res.status(400).send('Invalid password');
+      // }else {
+        // const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
+        // res.json({ token });
+      // }
     }
   } catch (error) {
     next(error);
